62d51bbe0c74fc2ea4163c4048d6399d00b59c5c
fdavey22 <S00170902@mail.itsligo.ie>
2019-08-20 21:10:27 +0100

    Basic search

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,13 +10,14 @@ import { IBook } from "../shared/ibook";
 export class HomeComponent implements OnInit {
   books: IBook[] = [];
   errorMessage: string;
+  query: string;
 
   constructor(private _bookservice: BookService) {}
 
   ngOnInit() {}
 
   search() {
-    this._bookservice.search("Pride and Prejudice").subscribe(
+    this._bookservice.search(this.query).subscribe(
       (result: IBook[]) => {
         this.books = result;
       },
